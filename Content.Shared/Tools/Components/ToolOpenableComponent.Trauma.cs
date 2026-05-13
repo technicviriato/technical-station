b7b7a6a0bc01a185bafd3a37019b7cc88c219f89
TechnicFighter <tomgodmartins@gmail.com>
2026-05-13 19:24:03 +0100

    address reviews

--- Content.Shared/Tools/Components/ToolOpenableComponent.Trauma.cs
+++ Content.Shared/Tools/Components/ToolOpenableComponent.Trauma.cs
@@ -3,7 +3,7 @@
 public sealed partial class ToolOpenableComponent
 {
     /// <summary>
-    ///     If false, will not show examine when examined
+    ///     If true, will not show examine when examined
     /// </summary>
     [DataField, AutoNetworkedField]
     public bool ShowExamine;
