d6334a62d817b2dd6f2d18332804fb297362c4f3
TechnicFighter <tomgodmartins@gmail.com>
2026-06-15 21:11:16 +0100

    im no longer a c# noob

--- Content.Trauma.Shared/Jobs/LawyerComponent.cs
+++ Content.Trauma.Shared/Jobs/LawyerComponent.cs
@@ -3,4 +3,4 @@
 namespace Content.Trauma.Shared.Jobs;
 
 [RegisterComponent, NetworkedComponent]
-public sealed partial class LawyerComponent : Component { }
+public sealed partial class LawyerComponent : Component;
