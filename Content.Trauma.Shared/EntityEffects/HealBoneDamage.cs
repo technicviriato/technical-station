94ddd3365854d31fd081b1676dd93f6e6c3c6ef5
TechnicFighter <tomgodmartins@gmail.com>
2026-06-21 03:47:23 +0100

    Ichor heals bone damage

---
+++ Content.Trauma.Shared/EntityEffects/HealBoneDamage.cs
@@ -0,0 +1,39 @@
+﻿using Content.Medical.Shared.Traumas;
+using Content.Medical.Shared.Wounds;
+using Content.Shared.Body;
+using Content.Shared.EntityEffects;
+using Content.Shared.FixedPoint;
+
+namespace Content.Trauma.Shared.EntityEffects;
+
+/// <summary>
+/// Heals bone damage on every body part with a bone.
+/// </summary>
+public sealed partial class HealBoneDamageEntityEffectSystem : EntityEffectSystem<BodyComponent, HealBoneDamage>
+{
+    [Dependency] private TraumaSystem _trauma = default!;
+    [Dependency] private BodySystem _body = default!;
+
+    protected override void Effect(Entity<BodyComponent> entity, ref EntityEffectEvent<HealBoneDamage> args)
+    {
+        var amount = FixedPoint2.New(args.Effect.Amount * args.Scale);
+
+        foreach (var woundable in _body.GetOrgans<WoundableComponent>(entity.Owner))
+            _trauma.HealBone(woundable, amount);
+    }
+}
+
+/// <summary>
+/// Used in yaml to set how much bone damage a reagent or chemical heals.
+/// </summary>
+public sealed partial class HealBoneDamage : EntityEffectBase<HealBoneDamage>
+{
+    /// <summary>
+    /// Amount of bone integrity restored.
+    /// </summary>
+    [DataField]
+    public float Amount = 1.0f;
+
+    public override string EntityEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
+        => Loc.GetString("entity-effect-guidebook-heal-bone-damage", ("chance", Probability));
+}
